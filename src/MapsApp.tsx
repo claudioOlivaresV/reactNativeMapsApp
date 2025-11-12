49d4bb5f7fd2b1c1980986f9cdf21654a473fefc
claudioOlivaresV <ca.olivares91@gmail.com>
2025-11-12 20:11:39 -0300

    feat: cambio home

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,21 +1,10 @@
+import { Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-
-import {enableLatestRenderer} from 'react-native-maps';
-import {NavigationContainer} from '@react-navigation/native';
-import {StackNavigator} from './presentation/navigation/StackNavigator';
-import { PermissionsChecker } from './presentation/providers/PermissionsChecker';
-
-
-
-enableLatestRenderer();
-
 
 export const MapsApp = () => {
   return (
-    <NavigationContainer>
-      <PermissionsChecker>
-        <StackNavigator />
-      </PermissionsChecker>
-    </NavigationContainer>
+    <View>
+      <Text>MapsApp</Text>
+    </View>
   );
 };
